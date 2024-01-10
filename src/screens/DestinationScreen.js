7763c603e690f20b0ad390732d668ac13c92dca9
Vihar4104 <tvjcoder414@gmail.com>
2024-01-10 18:23:11 +0530

    commited

--- src/screens/DestinationScreen.js
+++ src/screens/DestinationScreen.js
@@ -12,7 +12,7 @@ import {
   FlatList,
   Image,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { LinearGradient } from 'expo-linear-gradient';
 
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import * as Animatable from "react-native-animatable";
@@ -24,6 +24,7 @@ import { useNavigation } from "@react-navigation/native";
 import { ChevronLeftIcon, HeartIcon } from "react-native-heroicons/solid";
 import { theme } from "../theme";
 import { styled } from "nativewind";
+import logoImage from "../../assets/images/logo.png";
 
 const ios = Platform.OS == "ios";
 
@@ -79,8 +80,9 @@ export default function DestinationScreen(props) {
             style={{
               width: wp(88),
               margin: 20,
-
-              borderRadius: 40,
+              
+              borderRadius:40,
+              
             }}
             ref={flatListRef}
             data={images}
@@ -93,6 +95,7 @@ export default function DestinationScreen(props) {
               {
                 useNativeDriver: false,
                 listener: handleScroll,
+                
               }
             )}
             renderItem={({ item }) => (
@@ -106,10 +109,12 @@ export default function DestinationScreen(props) {
                   height: hp(40),
                   marginBottom: -200,
                   overflow: "hidden",
-
+                  
+                  
                   borderRadius: 30,
-
-                  margin: 10,
+                  
+                  margin:10
+                 
                 }}
               />
             )}
@@ -195,227 +200,334 @@ export default function DestinationScreen(props) {
             showsVerticalScrollIndicator={false}
             style={{ padding: wp(5), paddingBottom: hp(1) }}
           >
-            <Animatable.View
-              animation="fadeInUpBig"
-              delay={200}
-              style={{
-                backgroundColor: "#e3e3e3", // Adjust background color as needed
-                borderRadius: wp(5), // Slightly increased border radius
-                padding: wp(4),
-                marginBottom: hp(3),
-                shadowColor: "#fff",
-                shadowOffset: { width: 0, height: 5 },
-                shadowOpacity: 0.3,
-                shadowRadius: 8,
-                elevation: 8, // For Android shadow
-                borderWidth: 1, // Border width
-                borderColor: "#ccc",
-                // Border color
-              }}
-            >
-              <LinearGradient
-                colors={["#e6e6e6", "#e3e3e3"]} // Adjust gradient colors as needed
-                style={{
-                  flex: 1,
-                  borderRadius: wp(6), // Slightly increased border radius
-                  padding: wp(4),
+           <Animatable.View
+  animation="fadeInUpBig"
+  delay={200}
+  style={{
+    backgroundColor: "#e3e3e3", // Adjust background color as needed
+    borderRadius: wp(5), // Slightly increased border radius
+    padding: wp(4),
+    marginBottom: hp(3),
+    shadowColor: '#fff',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8, // For Android shadow
+    borderWidth: 1, // Border width
+    borderColor: "#ccc",
+     // Border color
+  }}
+>
+  <LinearGradient
+    colors={['#e6e6e6', '#e3e3e3']} // Adjust gradient colors as needed
+    style={{
+      flex: 1,
+      borderRadius: wp(6), // Slightly increased border radius
+      padding: wp(4),
+     
+      alignItems: 'center',
+      
+  
+    }}
+  >
+    <AnimatedText
+      style={{
+        fontSize: wp(3.7),
+        color: theme.textDark,
+        marginTop: hp(0.1),
+        textAlign: 'justify',
+        lineHeight: hp(2.5),
+        letterSpacing:0.5
+      }}
+    >
+      {item?.longDescription}
+    </AnimatedText>
+  </LinearGradient>
+</Animatable.View>
 
-                  alignItems: "center",
-                }}
-              >
-                <AnimatedText
-                  style={{
-                    fontSize: wp(3.7),
-                    color: theme.textDark,
-                    marginTop: hp(0.1),
-                    textAlign: "justify",
-                    lineHeight: hp(2.5),
-                    letterSpacing: 0.5,
-                  }}
-                >
-                  {item?.longDescription}
-                </AnimatedText>
-              </LinearGradient>
-            </Animatable.View>
 
-            <Animatable.View
-              animation="fadeInUpBig"
-              delay={200}
-              style={{
-                backgroundColor: "#e3e3e3",
-                borderRadius: wp(3),
-                padding: wp(4),
-                marginBottom: hp(3),
-                flexDirection: "row", // Use a flex container to align text and icon horizontally
-                alignItems: "center",
-                backgroundColor: "#e3e3e3", // Adjust background color as needed
-                borderRadius: wp(5), // Slightly increased border radius
-                padding: wp(4),
-                marginBottom: hp(3),
-                shadowColor: "#fff",
-                shadowOffset: { width: 0, height: 5 },
-                shadowOpacity: 0.3,
-                shadowRadius: 8,
-                elevation: 8, // For Android shadow
-                borderWidth: 1, // Border width
-                borderColor: "#ccc", // Align items vertically in the flex container
-              }}
-            >
-              {/* Location Icon or Image */}
-              <Image
-                source={require("../../assets/images/ho.png")} // Replace with the actual path to your location icon/image
-                style={{
-                  width: wp(15), // Adjust the width of the image/icon as needed
-                  height: wp(12), // Adjust the height of the image/icon as needed
-                  marginRight: wp(1),
-                  marginLeft: -10,
-                  marginBottom: -15, // Adjust the margin between icon/image and text
-                }}
-              />
+<Animatable.View
+  animation="fadeInUpBig"
+  delay={200}
+  style={{
+    backgroundColor: "#e3e3e3",
+    borderRadius: wp(3),
+    padding: wp(4),
+    marginBottom: hp(3),
+    flexDirection: "row", // Use a flex container to align text and icon horizontally
+    alignItems: "center", 
+    backgroundColor: "#e3e3e3", // Adjust background color as needed
+    borderRadius: wp(5), // Slightly increased border radius
+    padding: wp(4),
+    marginBottom: hp(3),
+    shadowColor: '#fff',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8, // For Android shadow
+    borderWidth: 1, // Border width
+    borderColor: "#ccc",// Align items vertically in the flex container
+  }}
+>
+  {/* Location Icon or Image */}
+  <Image
+    source={require("../../assets/images/ho.png")} // Replace with the actual path to your location icon/image
+    style={{
+      width: wp(15), // Adjust the width of the image/icon as needed
+      height: wp(12), // Adjust the height of the image/icon as needed
+      marginRight: wp(1),
+      marginLeft:-10,
+      marginBottom:-15 // Adjust the margin between icon/image and text
+    }}
+  />
 
-              {/* Text Content */}
-              <View>
-                <Text
-                  style={{
-                    fontSize: wp(5),
-                    fontWeight: "bold",
-                    color: theme.textDark,
-                  }}
-                >
-                  Location
-                </Text>
-                <Text
-                  style={{
-                    fontSize: wp(3.7),
-                    color: theme.textDark,
-                    marginTop: hp(0.1),
-                    textAlign: "justify",
-                    lineHeight: hp(2.5),
-                    letterSpacing: 0.5,
-                  }}
-                >
-                  {item?.location?.city}, {item?.location?.state},{" "}
-                  {item?.location?.country}
-                </Text>
-              </View>
-            </Animatable.View>
+  {/* Text Content */}
+  <View>
+    <Text
+      style={{
+        fontSize: wp(5),
+        fontWeight: "bold",
+        color: theme.textDark,
+      }}
+    >
+      Location
+    </Text>
+    <Text style={{ fontSize: wp(3.7),
+        color: theme.textDark,
+        marginTop: hp(0.1),
+        textAlign: 'justify',
+        lineHeight: hp(2.5),
+        letterSpacing:0.5}}>
+      {item?.location?.city}, {item?.location?.state},{" "}
+      {item?.location?.country}
+    </Text>
+  </View>
+</Animatable.View>
 
             {/* Activities Section */}
             <Animatable.View
-              animation="fadeInUpBig"
-              delay={600}
-              style={{
-                backgroundColor: "#D3d3d3",
-                marginBottom: hp(3),
-                borderRadius: wp(3),
-                padding: wp(4),
-                marginTop: hp(0.1),
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: wp(5),
-                  fontWeight: "bold",
-                  color: theme.textLight,
-                }}
-              >
-                Activities
-              </Text>
-              {item?.activities?.map((activity, index) => (
-                <View
-                  key={index}
-                  style={{ flexDirection: "row", alignItems: "center" }}
-                >
-                  <FontAwesome
-                    name="circle"
-                    size={wp(4)}
-                    color={theme.textLight}
-                    style={{ marginRight: wp(2) }}
-                  />
-                  <Text style={{ fontSize: wp(4), color: theme.textLight }}>
-                    {activity}
-                  </Text>
-                </View>
-              ))}
-            </Animatable.View>
+  animation="fadeInUpBig"
+  delay={600}
+  style={{
+    backgroundColor: "#e3e3e3", // Lighter background color
+    borderRadius: wp(8),
+    padding: wp(6),
+    marginBottom: hp(3),
+    alignItems: "flex-start", // Align text to the left
+    shadowColor: "#fff",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8,
+    borderWidth: 1,
+    borderColor: "#ccc", // Light border color
+  }}
+>
+  <Text
+    style={{
+      fontSize: wp(6),
+      fontWeight: "bold",
+      color: "#333", // Darker text color
+      marginBottom: hp(2),
+    }}
+  >
+    Activities
+  </Text>
+  <View
+    style={{
+      flexWrap: "wrap",
+      flexDirection: "row",
+      alignItems: "center",
+    }}
+  >
+    {item?.activities?.map((activity, index) => (
+      <View
+        key={index}
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          fontColor:'white',
+          marginVertical: hp(1),
+          backgroundColor: index % 2 === 0 ? "#999999" : "#999999", // Alternating background colors
+          padding: wp(2),
+          borderRadius: wp(4),
+          marginRight: wp(2),
+        }}
+      >
+        <View
+          style={{
+            width: wp(3),
+            height: wp(3),
+            borderRadius: wp(2),
+            backgroundColor: "#000", // Circle color
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: wp(1),
+          }}
+        >
+          <FontAwesome name="circle" size={wp(1.5)} color="#FFF" />
+        </View>
+        <Text style={{ fontSize: wp(4.5), color: "#f2f2f2" }}>{activity}</Text>
+      </View>
+    ))}
+  </View>
+</Animatable.View>
+
+
+
 
             {/* Amenities Section */}
             <Animatable.View
-              animation="fadeInUpBig"
-              delay={800}
-              style={{
-                backgroundColor: "#D3d3d3",
-                marginBottom: hp(3),
-                borderRadius: wp(3),
-                padding: wp(4),
-                marginTop: hp(0.1),
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: wp(5),
-                  fontWeight: "bold",
-                  color: theme.textLight,
-                }}
-              >
-                Amenities
-              </Text>
-              {item?.amenities?.map((amenity, index) => (
-                <View
-                  key={index}
-                  style={{ flexDirection: "row", alignItems: "center" }}
-                >
-                  <FontAwesome
-                    name="circle"
-                    size={wp(4)}
-                    color={theme.textLight}
-                    style={{ marginRight: wp(2) }}
-                  />
-                  <Text style={{ fontSize: wp(4), color: theme.textLight }}>
-                    {amenity}
-                  </Text>
-                </View>
-              ))}
-            </Animatable.View>
+  animation="fadeInUpBig"
+  delay={600}
+  style={{
+    backgroundColor: "#e3e3e3", // Lighter background color
+    borderRadius: wp(8),
+    padding: wp(6),
+    marginBottom: hp(3),
+    alignItems: "flex-start", // Align text to the left
+    shadowColor: "#fff",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8,
+    borderWidth: 1,
+    borderColor: "#ccc", // Light border color
+  }}
+>
+  <Text
+    style={{
+      fontSize: wp(6),
+      fontWeight: "bold",
+      color: "#333", // Darker text color
+      marginBottom: hp(2),
+    }}
+  >
+  Amenities
+  </Text>
+  <View
+    style={{
+      flexWrap: "wrap",
+      flexDirection: "row",
+      alignItems: "center",
+    }}
+  >
+  {item?.amenities?.map((amenity, index) => (
+      <View
+        key={index}
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          fontColor:'white',
+          marginVertical: hp(1),
+          backgroundColor: index % 2 === 0 ? "#999999" : "#999999", // Alternating background colors
+          padding: wp(2),
+          borderRadius: wp(4),
+          marginRight: wp(2),
+        }}
+      >
+        <View
+          style={{
+            width: wp(3),
+            height: wp(3),
+            borderRadius: wp(2),
+            backgroundColor: "#000", // Circle color
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: wp(1),
+          }}
+        >
+          <FontAwesome name="circle" size={wp(1.5)} color="#FFF" />
+        </View>
+        <Text style={{ fontSize: wp(4.5), color: "#f2f2f2" }}>{amenity}</Text>
+      </View>
+    ))}
+  </View>
+</Animatable.View>
+
+
+
+
+
+
 
             {/* Reviews Section */}
             <Animatable.View
-              animation="fadeInUpBig"
-              delay={1000}
-              style={{
-                backgroundColor: "#D3d3d3",
-                marginBottom: hp(3),
-                borderRadius: wp(3),
-                padding: wp(4),
-                marginTop: hp(0.1),
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: wp(5),
-                  fontWeight: "bold",
-                  color: theme.textLight,
-                }}
-              >
-                Reviews
-              </Text>
-              {item?.reviews?.map((review, index) => (
-                <View key={index} style={{ marginBottom: hp(2) }}>
-                  <Text
-                    style={{
-                      fontSize: wp(4),
-                      color: theme.textLight,
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {review.user}
-                  </Text>
-                  <Text style={{ fontSize: wp(3.5), color: theme.textLight }}>
-                    {review.comment}
-                  </Text>
-                </View>
-              ))}
-            </Animatable.View>
+  animation="fadeInUpBig"
+  delay={1000}
+  style={{
+    backgroundColor: "#e3e3e3",
+    borderRadius: wp(3),
+    // padding: wp(4),
+    marginBottom: hp(3),
+    // flexDirection: "row", // Use a flex container to align text and icon horizontally
+   
+    backgroundColor: "#e3e3e3", // Adjust background color as needed
+    borderRadius: wp(5), // Slightly increased border radius
+    // padding: wp(4),
+    marginBottom: hp(3),
+    shadowColor: '#fff',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8, // For Android shadow
+    borderWidth: 1, 
+    borderColor: "#ccc"// Border width
+  }}
+>
+  <Text
+    style={{
+      fontSize: wp(5),
+      fontWeight: "bold",
+      marginBottom:10,
+      backgroundColor:'#e9e9e9',
+      width:220,
+      height:50,
+      padding:10,
+      marginLeft:75,
+      borderRadius:wp(5)
+
+    }}
+  >
+    Reviews and Ratings
+  </Text>
+  {item?.reviews?.map((review, index) => (
+    <View key={index} style={{ marginBottom: hp(2), flexDirection: "row" }}>
+      {/* Display Author's Profile Picture */}
+      <Image
+        source={logoImage}
+        style={{
+          width: wp(21),
+          height: wp(20),
+          borderRadius: wp(4),
+          marginRight: wp(2),
+        }}
+      />
+      <View style={{ flex: 1 }}>
+        <Text
+          style={{
+            fontSize: wp(4),
+            color: theme.textLight,
+            fontWeight: "bold",
+            marginBottom: hp(1),
+          }}
+        >
+          {review.user}
+        </Text>
+        <Text style={{ fontSize: wp(3.5), color: theme.textLight }}>
+          {review.comment}
+        </Text>
+        {/* Display Ratings (Assuming rating is a number out of 5) */}
+        <View style={{ flexDirection: "row", alignItems: "center", marginTop: hp(1) }}>
+          <FontAwesome name="star" size={wp(4)} color="#FFD700" style={{ marginRight: wp(1) }} />
+          <Text style={{ fontSize: wp(4), color: theme.textLight }}>{review.rating}/5</Text>
+        </View>
+      </View>
+    </View>
+  ))}
+</Animatable.View>
+
+
             <Animatable.View
               animation="fadeInUpBig"
               delay={1200}
@@ -440,7 +552,7 @@ export default function DestinationScreen(props) {
                     color: "white",
                   }}
                 >
-                  Book now
+                  Direction
                 </Text>
               </TouchableOpacity>
             </Animatable.View>
